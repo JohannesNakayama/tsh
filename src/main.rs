7f733714abff78c25e9d0af92e16dbba429a1c49
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2025-07-06 14:02:56 +0200

    feat: apply migrations on startup, clean up main script

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,20 @@
 use std::error::Error;
 use tsh::{
-    llm::LlmClient,
-    tui::app::{self, App},
+    db::migrate_to_latest, llm::LlmClient, tui::app::{self, App}
 };
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     // TODO: load from config, not env
-    // let db_url = std::env::var("DATABASE_URL")?;
+    let db_url = std::env::var("DATABASE_URL")?;
     let api_base = std::env::var("API_BASE")?;
     let api_key = std::env::var("API_KEY")?;
     let embedding_model = std::env::var("EMBEDDINGS_MODEL")?;
     let chat_model = std::env::var("CHAT_MODEL")?;
 
-    // TODO: is it a good idea to run this every time?
-    // migrate_to_latest(&db_url).await?;
+    migrate_to_latest(&db_url).await?;
 
     let llm_client = LlmClient::new(api_base, api_key, embedding_model, chat_model);
-
-    // let mut conn = get_db(&db_url).await?;
-    // let tx = conn.transaction()?;
-    // let parent = find_zettel_by_id(&tx, 1).await?;
-    // tx.commit()?;
-    // add_zettel(&mut llm_client, &vec![]).await?;
-    // chat().await?;
-    // add_combined_zettel(&mut llm_client).await?;
 
     color_eyre::install()?; // TODO: where best to call this?
     let mut tsh_app = App::new(llm_client);
