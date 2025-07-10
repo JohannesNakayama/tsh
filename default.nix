7a45a12e354c59c959c86f5c00f5608a25016640
Johannes Nakayama <johannes.nakayama@rwth-aachen.de>
2025-07-10 23:33:34 +0200

    chore(nix): install nix package from github, not from local repo

--- default.nix
+++ default.nix
@@ -1,7 +1,27 @@
 { pkgs ? import <nixpkgs> { } }:
+
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "tsh";
   version = "0.1.0";
-  cargoLock.lockFile = ./Cargo.lock;
-  src = ./.;
+
+  src = pkgs.fetchFromGitHub {
+    owner = "JohannesNakayama";
+    repo = "tsh";
+    rev = "main";
+    hash = "sha256-hTYnP3sMyskrvMrNooO9MZhJurrmb9NfERKZzUfQsHs=";
+  };
+
+  cargoHash = "sha256-U4/7VqA1pWov2DR76KYfoZKydQMXyh7bPTUlfvHyHQI=";
+
+  buildInputs = [];
+
+  nativeBuildInputs = [];
+
+  meta = with pkgs.lib; {
+    description = "A simple tool to help you think";
+    homepage = "https://github.com/JohannesNakayama/tsh";
+    license = licenses.asl20;
+    platforms = platforms.linux;
+    maintainers = "Johannes Nakayama";
+  };
 }
